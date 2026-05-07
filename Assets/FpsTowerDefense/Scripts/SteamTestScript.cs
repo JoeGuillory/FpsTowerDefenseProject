febf3b9ae0e67171d460deff3918fd18cef8ecd5
Joe Guillory <joeguillory15@gmail.com>
2026-05-06 20:25:15 -0500

    Messed with the steam join but found another way to use steam
    Use steam to store the code from unity relay to then join the game

--- Assets/FpsTowerDefense/Scripts/SteamTestScript.cs
+++ Assets/FpsTowerDefense/Scripts/SteamTestScript.cs
@@ -1,24 +1,71 @@
 using Steamworks;
+using Unity.Netcode;
 using UnityEngine;
 
 public class SteamTestScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected Callback<LobbyCreated_t> LobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> GameLobbyJoinRequested;
+    protected Callback<LobbyEnter_t> LobbyEnter;
+
+
+    public ulong CurrentLobbyId;
+    private const string HostAddressKey = "HostAddress";
+    
+    [SerializeField]
+    private NetworkManager _networkManager;
+    
+    
+    
     void Start()
     {
-        if(SteamManager.Initialized)
+        if (!SteamManager.Initialized)
         {
-            var lobby = SteamFriends.GetPersonaName();
-            Debug.Log(lobby);
+            return;
         }
         
-
-
+        LobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        GameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnLobbyJoined);
+        LobbyEnter = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HostLobby()
     {
-        
+        Debug.Log("Button Pressed ");
+        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 3);
     }
+
+
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if (callback.m_eResult != EResult.k_EResultOK)
+        {
+            return;
+        }
+        Debug.Log("Started Lobby");
+        _networkManager.StartHost();
+
+        SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey,SteamUser.GetSteamID().ToString());
+
+    }
+
+    private void OnLobbyJoined(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        CurrentLobbyId = callback.m_ulSteamIDLobby;
+
+
+        if (NetworkManager.Singleton.IsServer)
+        {
+            return;
+        }
+        
+        _networkManager.StartClient();
+    }
+    
+    
 }
